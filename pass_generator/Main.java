28f46b9a153622e45ab7b1970741343c22bea7cf
william winsett <wrwinsett@gmail.com>
2023-05-05 16:01:14 -0400

    added new variable to increase randomness

--- pass_generator/Main.java
+++ pass_generator/Main.java
@@ -11,31 +11,38 @@ public class Main {
 		String favoriteSong = favSong.nextLine();
 		//code asks user for input, turns that input with a string
 		
+		Scanner oldPet = new Scanner(System.in);
+		System.out.println("Who was your childhood pet?" );
+		String childhoodPet = oldPet.nextLine();
+		//code asks user for input, turns that input with a string
+		
 		Scanner favNumb = new Scanner(System.in);
 		System.out.println("What is your favorite number?");
 		String favoriteNumber = favNumb.nextLine();
 		
 		Scanner houseNumb = new Scanner(System.in);
-		System.out.println("What is your favorite number?");
+		System.out.println("What was the number of your childhood home?");
 		String houseNumber = houseNumb.nextLine();
 		
-		String newPass = combine(favoriteNumber, favoriteSong, houseNumber);
+		String newPass = combine(favoriteNumber, favoriteSong, houseNumber, childhoodPet);
 		
 		System.out.println(newPass);
 		
 	}
 	
-	public static String combine(String s1, String s2, String s3) {
+	public static String combine(String s1, String s2, String s3, String s4) {
 		Random passChoice = new Random();
-		int pick = passChoice.nextInt();
+		int pick = passChoice.nextInt(5);
 		
 		switch(pick) {
 		case 0:
-			return s1 + s2 + s3;
+			return s1 + s2 + s4 + s3;
 		case 1:
 			return s3 + s2 + s1;
 		case 2:
 			return s3 + "." + s2 + s1;
+		case 3:
+			return s3 + s4 + s2 +s1;
 		default:
 			return s1 + "." + s2 + "." + s3 + "!";
 		}
