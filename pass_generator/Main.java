dd2e89687b4b21ac5174817eb1bab8f3b7130a88
william winsett <wrwinsett@gmail.com>
2023-05-05 09:35:31 -0400

    added function to ask for song

--- pass_generator/Main.java
+++ pass_generator/Main.java
@@ -1,10 +1,15 @@
 package pass_generator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner favSong = new Scanner(System.in);
+		System.out.println("What was your favorite song last month?" );
+		String favoriteSong = favSong.nextLine();
+		//code asks user for input, turns that input with a string
+		
 	}
 
 }
