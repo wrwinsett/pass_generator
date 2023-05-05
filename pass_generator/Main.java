017467ff6ae6af6e1bf750f24d49a3b5db38a9ff
william winsett <wrwinsett@gmail.com>
2023-05-05 13:36:55 -0400

    added favorite number/house number, and basic output

--- pass_generator/Main.java
+++ pass_generator/Main.java
@@ -10,6 +10,16 @@ public class Main {
 		String favoriteSong = favSong.nextLine();
 		//code asks user for input, turns that input with a string
 		
+		Scanner favNumb = new Scanner(System.in);
+		System.out.println("What is your favorite number?");
+		String favoriteNumber = favNumb.nextLine();
+		
+		Scanner houseNumb = new Scanner(System.in);
+		System.out.println("What is your favorite number?");
+		String houseNumber = houseNumb.nextLine();
+		
+		System.out.println("your new password is: " + favoriteNumber + favoriteSong + houseNumber + "!");
+		
 	}
 
 }
