c95aa7d825fc802a32983206606742d0146e418e
william winsett <wrwinsett@gmail.com>
2023-05-09 12:41:44 -0400

    add function to save pass as a text file

--- pass_generator/Main.java
+++ pass_generator/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Main {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Scanner favSong = new Scanner(System.in);
 		System.out.println("What was your favorite song last month?" );
