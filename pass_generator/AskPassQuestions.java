47371977fa3bcf284c6c0046e53ea207869ed8a2
william winsett <wrwinsett@gmail.com>
2023-05-23 14:26:27 -0400

    add questions to strengthen process

--- pass_generator/AskPassQuestions.java
+++ pass_generator/AskPassQuestions.java
@@ -11,11 +11,20 @@ public class AskPassQuestions {
     }
 
     public static String[] readUserInputs() {
-        String[] inputs = new String[4];
+    	System.out.println("Hey! We're gonna ask some questions to create a new password for you.");
+    	System.out.println("Please answer all questions in the form of one word or numerical answers.");
+    	
+    	 try {
+             // Pause execution for 1 second (1000 milliseconds)
+             Thread.sleep(1000);
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+    	
+        String[] inputs = new String[7];
 
         System.out.println("What was your favorite song last month?");
         inputs[0] = scanner.nextLine();
-		
 
         System.out.println("Who was your childhood pet?");
         inputs[1] = scanner.nextLine();
@@ -25,6 +34,15 @@ public class AskPassQuestions {
 
         System.out.println("What was the number of your childhood home?");
         inputs[3] = scanner.nextLine();
+        
+        System.out.println("Who was your first best friend?");
+        inputs[4] = scanner.nextLine();
+        
+        System.out.println("How many members are you in your family?");
+        inputs[5] = scanner.nextLine();
+        
+        System.out.println("What is your mother's maiden name?");
+        inputs[6] = scanner.nextLine();
 
         return inputs;
     }
